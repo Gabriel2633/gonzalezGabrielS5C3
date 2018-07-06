4ac63e4891fa7ae2622f6f792a634e538eb61b97
Gabriel2633 <40802589+Gabriel2633@users.noreply.github.com>
2018-07-06 16:46:30 +0000

    Add files via upload

---
+++ ProfeCorreccionNumeros.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdlib>
+
+
+using std::cout;
+using std::endl;
+
+
+int main()
+{
+  int i, numeroal;
+  for(i=0; i<1000; i++)
+	{
+    numeroal = rand() % 100 + 1;
+
+	if (numeroal%2==0 && numeroal<=89 && i <= 49) 
+{
+    cout << i << " " << numeroal << endl;  	
+} 	 
+        }	
+  return 0;
+}
+
