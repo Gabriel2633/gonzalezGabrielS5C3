78b03a5df8f7a54bded7ab54a4e7c62708e6b5da
Gabriel2633 <40802589+Gabriel2633@users.noreply.github.com>
2018-07-09 14:22:29 +0000

    Add files via upload

---
+++ GonzalezGabriel_s6C0.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdlib>
+
+int nPuntos(float x, float y, float H);
+
+int main()
+{
+	float h = 0.01;
+	float min_x = 0.0;
+	float max_x = 4.0;
+	float O;
+	O = nPuntos(max_x,min_x,h);
+	std::cout << "num de ptos es igual a " << O << std::endl;
+	
+	float x=n[O];
+	float y=u[O];
+
+	float n[0]= min_x
+	float u[0]= 1.0
+	
+	
+	return 0;
+}
+
+double nPuntos(float x, float y, float H)
+{
+	double q;
+	q = (x-y)/H;
+	return q;
+}
+
+double funci1(x,y)
+{
+	
+}
